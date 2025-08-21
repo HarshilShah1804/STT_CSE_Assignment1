01be86f619dc4bdf1b613221ef4e82b230a4a296
HarshilShah1804 <hkshah1978@gmail.com>
2025-08-21 11:53:52 +0530

    fix: fix pylint errors

--- pylint_check.py
+++ pylint_check.py
@@ -1,55 +1,59 @@
+"""
+Module for regex matching using memoized recursion.
+"""
+
 class Solution:
-    def isMatch(self, s: str, p: str) -> bool:
-        i, j = len(s) - 1, len(p) - 1
-        return self.backtrack({}, s, p, i, j)
+    """Class to check regex matching for two strings."""
 
-    def backtrack(self, cache, s, p, i, j):
-        key = (i, j)
-        if key in cache:
-            return cache[key]
+    def is_match(self, s: str, p: str) -> bool:
+        """
+        Check if the string `s` matches the pattern `p`.
+        Supports '.' (any single character) and '*' (zero or more of the preceding element).
+        """
+        cache = {}
 
-        if i == -1 and j == -1:
-            cache[key] = True
-            return True
-
-        if i != -1 and j == -1:
-            cache[key] = False
-            return cache[key]
-
-        if i == -1 and p[j] == '*':
-            k = j
-            while k != -1 and p[k] == '*':
-                k -= 2
-            
-            if k == -1:
-                cache[key] = True
-                return cache[key]
-            
-            cache[key] = False
-            return cache[key]
-        
-        if i == -1 and p[j] != '*':
-            cache[key] = False
-            return cache[key]
-
-        if p[j] == '*':
-            if self.backtrack(cache, s, p, i, j - 2):
-                cache[key] = True
-                return cache[key]
-            
-            if p[j - 1] == s[i] or p[j - 1] == '.':
-                if self.backtrack(cache, s, p, i - 1, j):
-                    cache[key] = True
-                    return cache[key]
-        
-            if p[j] == '.' or s[i] == p[j]:
-            if self.backtrack(cache, s, p, i - 1, j - 1):
-                cache[key] = True
+        def backtrack(i: int, j: int) -> bool:
+            """
+            Memoized recursive function for regex matching.
+            """
+            key = (i, j)
+            if key in cache:
                 return cache[key]
 
-        cache[key] = False
-        return cache[key]
+            result = False  # default
+
+            # Base cases
+            if i == -1 and j == -1:
+                result = True
+            elif j == -1:
+                result = False
+            elif i == -1:
+                if p[j] == '*':
+                    k = j
+                    while k >= 1 and p[k] == '*':
+                        k -= 2
+                    result = k < 0
+                else:
+                    result = False
+            elif p[j] == '*':
+                # '*' matches zero occurrences
+                result = backtrack(i, j - 2)
+                # '*' matches one or more occurrences
+                if not result and (p[j - 1] == s[i] or p[j - 1] == '.'):
+                    result = backtrack(i - 1, j)
+            else:
+                # Normal match
+                if s[i] == p[j] or p[j] == '.':
+                    result = backtrack(i - 1, j - 1)
+
+            cache[key] = result
+            return result
+
+        return backtrack(len(s) - 1, len(p) - 1)
+
 
 if __name__ == "__main__":
     obj = Solution()
-        print(obj.isMatch("aa", "a"))
+    print(obj.is_match("aa", "a"))    # False
+    print(obj.is_match("aa", "a*"))   # True
+    print(obj.is_match("ab", ".*"))   # True
